eb3a7e4da2b452ea4c43d06fc2995be619fdd47d
j <j@m.localhost>
2018-01-26 08:30:52 +0100

    order by ctime

--- controllers/draws.ts
+++ controllers/draws.ts
@@ -5,7 +5,13 @@ export const getDraws: express.RequestHandler = async (req, res) => {
     const page = (req.query.page) ? req.query.page : 0;
     const resultsPerPage = (req.query.resultsPerPage) ? req.query.resultsPerPage : 20;
 
-    const queryText = 'SELECT name, ctime FROM newdraws LIMIT $1 OFFSET $2';
+    const queryText = `
+    SELECT name, ctime
+    FROM newdraws
+    ORDER BY ctime DESC
+    LIMIT $1
+    OFFSET $2
+    `;
     const qr = await Client.query(
         queryText,
         [resultsPerPage, resultsPerPage * page],
